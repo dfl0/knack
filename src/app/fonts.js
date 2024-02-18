89ab1043e1b03ebf8ec191e98f32a34c78003f1f
dfl0 <dflocos@gmail.com>
2024-02-17 22:14:30 -0500

    Update config and global files

---
+++ src/app/fonts.js
@@ -0,0 +1,6 @@
+import { IBM_Plex_Sans } from 'next/font/google';
+
+export const ibm_plex_sans = IBM_Plex_Sans({
+  weight: ['300', '400', '700'],
+  subsets: ['latin']
+});
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import '@/app/global.css';
+import { ibm_plex_sans} from '@/app/fonts';
 
 export const metadata = {
   title: "App",
@@ -11,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ibm_plex_sans.className}>{children}</body>
     </html>
   );
 }
