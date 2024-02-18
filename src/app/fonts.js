2ad898211921e8e26bdafc4e73c31179f8354507
dfl0 <dflocos@gmail.com>
2024-02-18 06:44:14 -0500

    Add semibold font weight

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,6 +1,6 @@
 import { IBM_Plex_Sans } from 'next/font/google';
 
 export const ibm_plex_sans = IBM_Plex_Sans({
-  weight: ['300', '400', '700'],
+  weight: ['300', '400', '600', '700'],
   subsets: ['latin']
 });
