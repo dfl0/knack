06320ff70ff26589efbb96a054229ad499885361
dfl0 <dflocos@gmail.com>
2024-02-28 06:51:47 -0500

    remove rendundancies in navbar rendering

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -9,42 +9,36 @@ export default function NavBar() {
   const pathname = usePathname();
   let username = "student1"
   return (
-    <>
-      <div className="h-15 w-full bg-gray-200 fixed place-items-stretch">
-        <div className="flex items-center px-6 py-3 h-max">
-          <div className="text-2xl font-bold">
-            <Link href="/main/knacks">
-              KNACK
-            </Link>
-          </div>
-          <input type="text" placeholder="What are you looking for?" name="Search" className="appearance-none outline-none text-sm bg-gray-300 border px-4 rounded-xl h-8 grow mx-20" />
-          <div className="text-sm flex gap-6 mr-20 static">
-            <Link
-              href="/main/knacks"
-              className={ clsx({ 'border-solid border-b-[1px] border-b-gray-400 border-t-1 border-t-transparent': pathname === '/main/knacks' }) }
-            >
-              <span className="text-gray-500 pr-0.5">KNICK</span><span>KNACKS</span>
-            </Link>
-            <Link
-              href="/main/chat"
-              className={ clsx({ 'border-solid border-b-[1px] border-b-gray-400 border-t-1 border-t-transparent': pathname === '/main/chat' }) }
-            >
-              <span className="text-gray-500 pr-0.5">KNACK</span><span>CHATS</span>
-            </Link>
-          </div>
-          <div className="text-sm">{username}</div>
-          <div className="w-8 h-8 rounded-xl ml-2 overflow-hidden">
-            <Image
-              src="/images/default_pfp.png"
-              alt="Profile Picture"
-              width={32}
-              height={32}
-            />
-          </div>
-        </div>
+    <div className="flex items-center w-full h-14 px-6 bg-gray-200 fixed overflow-hidden">
+      <div className="text-2xl font-bold">
+        <Link href="/main/knacks">
+          KNACK
+        </Link>
       </div>
-      <div>
+      <input type="text" placeholder="What are you looking for?" name="Search" className="appearance-none outline-none text-sm bg-gray-300 border px-4 rounded-xl h-8 grow mx-20" />
+      <div className="text-sm flex gap-6 mr-20 static">
+        <Link
+          href="/main/knacks"
+          className={ clsx({ 'border-solid border-b-[1px] border-b-gray-400 border-t-1 border-t-transparent': pathname === '/main/knacks' }) }
+        >
+          <span className="text-gray-500 pr-0.5">KNICK</span><span>KNACKS</span>
+        </Link>
+        <Link
+          href="/main/chat"
+          className={ clsx({ 'border-solid border-b-[1px] border-b-gray-400 border-t-1 border-t-transparent': pathname === '/main/chat' }) }
+        >
+          <span className="text-gray-500 pr-0.5">KNACK</span><span>CHATS</span>
+        </Link>
       </div>
-    </>
+      <div className="text-sm">{username}</div>
+      <div className="w-8 h-8 rounded-xl ml-2 overflow-hidden">
+        <Image
+          src="/images/default_pfp.png"
+          alt="Profile Picture"
+          width={32}
+          height={32}
+        />
+      </div>
+    </div>
   );
 }
--- src/app/main/knacks/page.js
+++ src/app/main/knacks/page.js
@@ -3,7 +3,7 @@ import Preview from '@/app/components/preview';
 
 export default function Knacks() {
   return (
-    <div className='pt-6 px-20 flex items-stretch gap-16 justify-center flex-wrap overflow-clip'>
+    <div className='pt-6 px-20 flex items-stretch gap-16 justify-center flex-wrap '>
       <AddListing />
       <Preview photo="/images/img1.png" username="dflocos1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." price="25"/>
       <Preview photo="/images/img2.png" username="jhannag1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." price="25"/>
