21fd1da52e19a444d837ef73d1adbde32e190937
dfl0 <dflocos@gmail.com>
2024-03-05 20:43:43 -0500

    shorten shrink tag

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -3,7 +3,7 @@ import NavBar from '@components/navbar';
 export default function MainLayout({ children }) {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="w-full flex-shrink-0">
+      <div className="w-full shrink-0">
         <NavBar />
       </div>
       <div className="w-full h-full overflow-scroll">{children}</div>
