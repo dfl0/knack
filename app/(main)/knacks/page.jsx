a7c3269030aa98d3f7325cd4986fcd84db2f7179
dfl0 <dflocos@gmail.com>
2024-03-05 20:49:56 -0500

    add another listing

--- app/(main)/knacks/page.jsx
+++ app/(main)/knacks/page.jsx
@@ -9,6 +9,7 @@ export default function Knacks() {
       <Preview photo="/images/img2.png" username="jhannag1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." price="25"/>
       <Preview photo="/images/img3.png" username="mbronic1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." price="25"/>
       <Preview photo="/images/img4.png" username="username" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." price="25"/>
+      <Preview photo="/images/img5.png" username="another1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." price="25"/>
     </div>
   );
 }
