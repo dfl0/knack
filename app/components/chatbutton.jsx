001851af0e00c4a417ae2947a9738856f0fb6c9d
dfl0 <dflocos@gmail.com>
2024-04-01 16:14:07 -0400

    update chat button styling

--- app/components/chatbutton.jsx
+++ app/components/chatbutton.jsx
@@ -1,19 +1,19 @@
 import clsx from "clsx";
 
-export default function ChatButton({id, title, sub, photo, active, action}) {
+export default function ChatButton({ id, title, sub, photo, active, action }) {
   return (
     <button
       onClick={() => action(id)}
       className={clsx(
-        "w-full bg-gray-200 hover:bg-slate-300 px-2 py-4 rounded-xl",
-        active === id && "bg-slate-400 hover:bg-slate-400"
+        "w-full text-zinc-950 hover:bg-zinc-100 p-2 rounded-xl",
+        active === id && "bg-zinc-100",
       )}
     >
       <div className="flex items-center">
-        <div className="h-10 w-10 bg-gray-600 rounded-full"></div>
-        <div className="flex flex-col justify-center items-start pl-4">
-          <div className="text-sm text-black font-semibold">{title}</div>
-          <div className="text-xs text-slate-600 italic">{sub}</div>
+        <div className="h-8 w-8 bg-zinc-300 rounded-xl"></div>
+        <div className="flex flex-col justify-center items-start ml-2">
+          <div className="text-sm font-medium">{title}</div>
+          <div className="text-xs text-zinc-500">{sub}</div>
         </div>
       </div>
     </button>
