162c68a98f32651663111e9875b10dbaadefd930
dfl0 <dflocos@gmail.com>
2024-06-09 18:33:32 -0400

    add the ability to get the current user that is logged in

---
+++ app/actions/getcurrentuser.js
@@ -0,0 +1,25 @@
+import prisma from "@/app/libs/prismadb"
+
+import getCurrentSession from "@/app/actions/getcurrentsession"
+
+const getCurrentUser = async () => {
+  try {
+    const session = await getCurrentSession()
+
+    if (!session?.user?.email)
+      return null
+
+    const currentUser = await prisma.user.findUnique({
+      where: { email: session.user.email },
+    })
+
+    if (!currentUser)
+      return null
+
+    return currentUser
+  } catch (error) {
+    return null
+  }
+}
+
+export default getCurrentUser
