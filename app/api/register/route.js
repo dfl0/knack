af2911b4387e336284ce27efe42cf6cb4903b83e
dfl0 <dflocos@gmail.com>
2024-06-25 20:27:52 -0400

    use the explicit values for register fields

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -14,7 +14,11 @@ export async function POST(req) {
     const hashedPassword = await bcrypt.hash(password, 12)
 
     const user = await prisma.user.create({
-      data: { email, name, hashedPassword },
+      data: {
+        name: name,
+        email: email,
+        hashedPassword: hashedPassword,
+      },
     })
 
     return NextResponse.json(user)
