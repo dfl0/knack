4451127e1e15c69e34e5dd57f73c333eeec2bf40
dfl0 <dflocos@gmail.com>
2024-07-12 11:24:07 -0400

    temporarily fix real-time updating

--- app/(main)/profile/page.jsx
+++ app/(main)/profile/page.jsx
@@ -1,16 +1,8 @@
-import getCurrentUser from "@/app/actions/getcurrentuser"
-import getFriends from "@/app/actions/getfriends"
-
-import Friend from "@components/friend"
-import FriendRequest from "@components/friendrequest"
+import FriendsList from "@components/friendslist"
+import FriendRequestsList from "@components/friendrequestslist"
 import Divider from "@components/divider"
 
-export default async function Profile() {
-  const currentUser = await getCurrentUser()
-  const friends = await getFriends()
-
-  console.log(currentUser.friendIds)
-
+export default function Profile() {
   return (
     <div className="h-full w-full px-64 py-6">
       <div className="m-auto w-full text-lg font-medium text-zinc-950">
@@ -20,41 +12,12 @@ export default async function Profile() {
         <div className="w-full">
           <div className="mb-2 text-xs font-medium text-zinc-400">Friends</div>
           <Divider />
-          <div className="flex flex-col">
-            {friends.map((friend) => (
-              <div key={friend.id}>
-                <Friend user={friend} />
-                <Divider />
-              </div>
-            ))}
-          </div>
+          <FriendsList />
         </div>
         <div className="w-full">
           <div className="mb-2 text-xs font-medium text-zinc-400">Pending</div>
           <Divider />
-          <div className="flex flex-col">
-            {currentUser.incomingFriendRequests
-              .map((request) => (
-                <div key={request.sender.id}>
-                  <FriendRequest
-                    user={request.sender}
-                    type="incoming"
-                  />
-                  <Divider />
-                </div>
-              ))}
-            {currentUser.outgoingFriendRequests
-              .filter((request) => !request.accepted)
-              .map((request) => (
-                <div key={request.recipient.id}>
-                  <FriendRequest
-                    user={request.recipient}
-                    type="outgoing"
-                  />
-                  <Divider />
-                </div>
-              ))}
-          </div>
+          <FriendRequestsList />
         </div>
       </div>
     </div>
