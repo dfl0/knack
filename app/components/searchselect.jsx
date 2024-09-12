0e3fb74c87fcee82580e642c33b6adf5a0c69071
Dorin Flocos <dflocos@gmail.com>
2024-09-12 15:20:20 -0400

    change cursor on hover

--- app/components/searchselect.jsx
+++ app/components/searchselect.jsx
@@ -50,7 +50,7 @@ const SearchSelect = ({ placeholder, disabled, value, onChange, options, errors,
         container: () => className,
         control: ({ isFocused }) =>
           cn(
-            "max-h-8 appearance-none rounded-xl border border-zinc-200 text-sm shadow-none outline-none transition-colors hover:bg-zinc-50",
+            "max-h-8 appearance-none rounded-xl border border-zinc-200 text-sm shadow-none outline-none transition-colors hover:bg-zinc-50 hover:cursor-text",
             isFocused && "border-zinc-300 bg-zinc-100 hover:bg-zinc-100",
             errors && "border-rose-500 bg-red-50 hover:bg-red-50",
             errors && isFocused && "border-rose-500 bg-red-50",
@@ -62,7 +62,7 @@ const SearchSelect = ({ placeholder, disabled, value, onChange, options, errors,
         multiValue: () => cn("items-center gap-1 rounded-lg bg-zinc-200 py-1 pl-2 pr-1.5 ring-1 ring-inset ring-zinc-300"),
         multiValueRemove: () => cn("text-zinc-500 hover:text-zinc-900"),
         indicatorsContainer: () => cn("m-auto max-h-6 p-2"),
-        dropdownIndicator: () => cn("p-1 text-gray-500 hover:text-zinc-900"),
+        dropdownIndicator: () => cn("p-1 text-gray-500 hover:text-zinc-900 hover:cursor-pointer"),
         menu: () => cn("mt-1 rounded-xl border border-zinc-300 bg-white p-1 text-sm"),
         option: ({ isFocused, isSelected }) =>
           cn(
