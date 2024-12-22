e6ac681586b99cb265f4d05bb246b17767ea04df
Dorin Flocos <dflocos@gmail.com>
2024-12-21 23:11:15 -0500

    replace function that doesn't work in serverless environments

--- app/actions/getsignedurl.js
+++ app/actions/getsignedurl.js
@@ -18,12 +18,16 @@ const getSignedURL = async () => {
   const session = await getCurrentSession()
   if (!session.user) return null
 
-  // const fileKey = crypto.randomBytes(32).toString("hex")  // could also inclulde file type or object type in the future
-  const fileKey = "test-key"  // test if previous line is causing issues in production environment
+  // file key could also inclulde file type or object type in the future
+  const generateFileKey = (bytes = 32) => {
+    const array = new Uint8Array(bytes)
+    crypto.getRandomValues(array)
+    return [...array].map((b) => b.toString(16).padStart(2, "0")).join("")
+  }
 
   const putObjectCommand = new PutObjectCommand({
     Bucket: process.env.AWS_S3_BUCKET_NAME,
-    Key: fileKey,
+    Key: generateFileKey(),
   })
 
   const signedURL = await getSignedUrl(s3, putObjectCommand, {
